c5a2179054dc6fe317bfefd214a22035e4467e3d
anirudh-31 <betrabetanirudh@gmail.com>
2025-08-24 12:04:45 +0530

    Add footer' q

---
+++ areaverse/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <React.Fragment>
+        <footer>
+            &copy; 2025 AreaVerse. All rights reserved.
+        </footer>
+    </React.Fragment>
+  )
+}
+
+export default Footer
--- areaverse/src/index.js
+++ areaverse/src/index.js
@@ -7,6 +7,7 @@ import {
 import Landing from "./pages/Landing/Landing";
 import './index.css';
 import Navbar from "./components/nav/Navbar";
+import Footer from "./components/Footer/Footer";
 // import AuthPage from "./pages/Auth/AuthPage";
 
 const router = createBrowserRouter([
@@ -19,7 +20,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <Navbar />
+    <RouterProvider router={router} />    
+    <Footer />
+    
   </React.StrictMode>
 );
